a9a2c05db1d2c9839a30b9d75f9294724654fd18
cristianopp <cristiano.patricio@ipg.pt>
2026-08-04 15:07:32 +0100

    Update website

--- assets/wedding/counter.js
+++ assets/wedding/counter.js
@@ -24,7 +24,7 @@ const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
 
-const counterRef = doc(db, "website", "stats");
+const counterRef = doc(db, "wedding-stats", "website-stats");
 
 const counterElement = document.getElementById("visitor-count");
 
